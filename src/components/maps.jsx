a2fa71b60fd698001117551fb453d3f35325f8d1
hamza53p <champak.official@gmail.com>
2024-08-09 16:25:14 +0530

    v6

--- src/components/maps.jsx
+++ src/components/maps.jsx
@@ -51,15 +51,9 @@ const Map = ({ selectedRow, highlightType }) => {
       try {
         const [peninsulaResponse, stateResponse, districtResponse] =
           await Promise.all([
-            fetch(
-              "./india-flood-database/assets/India_States_simplified.geojson"
-            ),
-            fetch(
-              "./india-flood-database/assets/India_States_simplified.geojson"
-            ),
-            fetch(
-              "./india-flood-database/assets/India_Districts_simplified.geojson"
-            ),
+            fetch("/assets/India_States_simplified.geojson"),
+            fetch("/assets/India_States_simplified.geojson"),
+            fetch("/assets/India_Districts_simplified.geojson"),
           ]);
 
         const peninsulaData = await peninsulaResponse.json();
